bda046b99b366fc077c054686d78ba91d904363b
Benjamin Sebastian <142651700+SebastianBenjamin@users.noreply.github.com>
2025-03-06 21:16:30 +0530

    Version latest

---
+++ PrakruthiAromaDiffuser_V2/secrets.h
@@ -0,0 +1,5 @@
+// Firebase Credentials
+#define API_KEY "AIzaSyCZsHJnSoTnZQkTK_ia2ZBD-ZEoJW09thM"
+#define DATABASE_URL "smartenergycontrolsystem-default-rtdb.firebaseio.com"
+#define USER_EMAIL "benjaminsebastian4db@gmail.com"
+#define USER_PASSWORD "database4benjamin"
